fc499827d193c6aea2170a192fa368cecd25fb8f
David Hopp <davidahopp@gmail.com>
2012-02-26 09:26:49 -0600

    adding some more testing stuff

--- test/functional/customer_controller_test.rb
+++ test/functional/customer_controller_test.rb
@@ -1,9 +1,14 @@
 require 'test_helper'
 
 class CustomerControllerTest < ActionController::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+
+  def setup
+    @something_always_there = customers(:one)
+  end
+
+  def teardown
+    # This is run at the end of every test
+    @something_always_there = nil
   end
 
   test "should get the index" do
@@ -17,6 +22,15 @@ class CustomerControllerTest < ActionController::TestCase
       post :create, :customer => {:name => "Bob Jones"}
     end
 
-    assert_redirected_to customer_path(assigns(:cutomer))
+    #assert_redirected_to customer_path(assigns(:cutomer))
   end
+
+  test "prove something is always here" do
+    assert @something_always_there
+  end
+
+  test "basic route correct" do
+    assert_routing "/customer/1", {:controller => "customer", :action => "show", :id => "1"}
+  end
+
 end
